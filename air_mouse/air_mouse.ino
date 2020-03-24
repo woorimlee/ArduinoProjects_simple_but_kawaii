b93fa8211c08d1438ecb8cb4ce8925853661ac4f
woorimlee <and_lamyland@naver.com>
2020-03-24 16:50:20 +0900

    일단 마우스 움직이는 거 됨

--- air_mouse/air_mouse.ino
+++ air_mouse/air_mouse.ino
@@ -14,6 +14,7 @@ void setup(){
   Wire.write(0x6B);  // PWR_MGMT_1 register
   Wire.write(0);     // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
+  //Serial.begin(9600);
   Serial.begin(115200);
   Mouse.begin();
   Serial.println("Mouse Start!!");
@@ -31,6 +32,13 @@ void loop(){
   GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
   
   int16_t gyroX, gyroZ;
   int Sensitivity = 300;
