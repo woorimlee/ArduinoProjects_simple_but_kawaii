57b0d419dc27f8dbb3a44b2648a7604490286373
woorimlee <and_lamyland@naver.com>
2020-04-14 16:03:18 +0900

    사실상 완성

--- blind_rod/blind_rod.ino
+++ blind_rod/blind_rod.ino
@@ -1,56 +1,61 @@
+/****************************************************
+<시각 장애인을 위한 장애물 감지 발열 지팡이>
+1. 지팡이 하단부에 장착된 초음파 센서에서 장애물을 감지.
+2. 장애물이 있다면 부저를 울려 경고를 준다.
+3. 온습도 센서를 이용해 추운 날씨에 발열패드를 동작시킨다.
+****************************************************/
+
 #include "DHT.h"
+
 #define RELAYPIN 3
 #define DHTPIN 4
+#define ECHOPIN 12
+#define TRIGPIN 13
 #define DHTTYPE DHT11
+bool flag = true;
+
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println(F("DHTxx test!"));
   pinMode(RELAYPIN, OUTPUT);
-
+  pinMode(ECHOPIN, INPUT);
+  pinMode(TRIGPIN, OUTPUT);
   dht.begin();
 }
 
 void loop() {
-  delay(2000);
-
-  float h = dht.readHumidity();
+  digitalWrite(TRIGPIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGPIN, LOW);
+  int duration = pulseIn(ECHOPIN, HIGH); 
+  int distance = duration / 29 / 2;  
+  
+  Serial.print(distance);
+  Serial.println("cm");
+  if(distance <= 40) {
+    tone(8, 261.63, 500);
+  }
+  
   float t = dht.readTemperature();
-  float f = dht.readTemperature(true);
 
-  if (isnan(h) || isnan(t) || isnan(f)) {
+  if (isnan(t)) {
     Serial.println(F("Failed to read from DHT sensor!"));
-    return;
   }
   Serial.print(F("Temperature: "));
   Serial.print(t);
   Serial.println("%");
-  
-  if(t <= 40) {
-    digitalWrite(RELAYPIN, HIGH);
-    delay(100);
+
+  if(t <= 25 && flag == true) {
+    digitalWrite(RELAYPIN, LOW);
+    flag = false;
     Serial.println("=====");
   }
-  else {
-    digitalWrite(RELAYPIN, LOW);
-    delay(100);
+  else if(t > 40 && flag == false) {
+    digitalWrite(RELAYPIN, HIGH);
+    flag = true;
     Serial.println("-----");
   }
   
-  /*
-  float hif = dht.computeHeatIndex(f, h);
-  float hic = dht.computeHeatIndex(t, h, false);
-
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
-  Serial.print(F("%  Temperature: "));
-  Serial.print(t);
-  Serial.print(F("°C "));
-  Serial.print(f);
-  Serial.print(F("°F  Heat index: "));
-  Serial.print(hic);
-  Serial.print(F("°C "));
-  Serial.print(hif);
-  Serial.println(F("°F"));*/
+  delay(500);
 }
