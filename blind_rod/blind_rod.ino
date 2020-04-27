36c25a161aeec9bcfa4c02358c03d08e9865987b
woorimlee <and_lamyland@naver.com>
2020-04-27 20:06:58 +0900

    코드 조금 수정

--- blind_rod/blind_rod.ino
+++ blind_rod/blind_rod.ino
@@ -1,8 +1,8 @@
 /****************************************************
-<시각 장애인을 위한 장애물 감지 발열 지팡이>
-1. 지팡이 하단부에 장착된 초음파 센서에서 장애물을 감지.
-2. 장애물이 있다면 부저를 울려 경고를 준다.
-3. 온습도 센서를 이용해 추운 날씨에 발열패드를 동작시킨다.
+  <시각 장애인을 위한 장애물 감지 발열 지팡이>
+  1. 지팡이 하단부에 장착된 초음파 센서에서 장애물을 감지.
+  2. 장애물이 있다면 부저를 울려 경고를 준다.
+  3. 온습도 센서를 이용해 추운 날씨에 발열패드를 동작시킨다.
 ****************************************************/
 
 #include "DHT.h"
@@ -15,16 +15,20 @@
 #define TRIGPIN 13
 #define DHTTYPE DHT11
 //릴레이 핀 관리를 위한 변수
-bool flag = true;
+//bool flag = true;
+float HT = 37.0; //높은 온도
+float LT = 32.0; //낮은 온도
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
-  pinMode(RELAYPIN, OUTPUT); 
-  pinMode(ECHOPIN, INPUT); 
-  pinMode(TRIGPIN, OUTPUT); 
+  pinMode(RELAYPIN, OUTPUT);
+  pinMode(ECHOPIN, INPUT);
+  pinMode(TRIGPIN, OUTPUT);
   dht.begin();
+  digitalWrite(RELAYPIN, HIGH);
+  Serial.println("initialize, RELAY MODULE HIGH");
 }
 
 void loop() {
@@ -32,13 +36,13 @@ void loop() {
   digitalWrite(TRIGPIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIGPIN, LOW);
-  int duration = pulseIn(ECHOPIN, HIGH); 
-  int distance = duration / 29 / 2;  
-  
+  int duration = pulseIn(ECHOPIN, HIGH);
+  int distance = duration / 29 / 2;
+
   Serial.print(distance);
   Serial.println("cm");
   //근거리에 물체 감지시 부저 울리기
-  if(distance <= 40) {
+  if (distance <= 40) {
     tone(BUZZERPIN, 261.63, 500);
   }
 
@@ -51,19 +55,17 @@ void loop() {
   Serial.print(t);
   Serial.println("%");
 
-  //온도가 25도보다 낮다면 릴레이 모듈 전기 흐르게해서 지팡이 동작시키고
-  if(t <= 25 && flag == true) {
+  //온도가 32도보다 낮다면 릴레이 모듈 전기 흐르게해서 지팡이 동작시키고
+  if (t <= LT) {
     //LOW레벨 트리거 릴레이 이용함.
     digitalWrite(RELAYPIN, LOW);
-    flag = false;
     Serial.println("=====");
   }
   //너무 높은 온도가 되지 않게 적절한 온도에서 발열패드 전기 끊기
-  else if(t > 40 && flag == false) {
+  else if (t > HT) {
     digitalWrite(RELAYPIN, HIGH);
-    flag = true;
     Serial.println("-----");
   }
-  
+
   delay(500);
 }
