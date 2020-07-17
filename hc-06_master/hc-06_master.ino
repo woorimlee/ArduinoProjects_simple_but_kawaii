c879a41a943557330025ad62ca448b2e0e6eb921
woorimlee <and_lamyland@naver.com>
2020-07-17 12:45:26 +0900

    ?

--- hc-06_master/hc-06_master.ino
+++ hc-06_master/hc-06_master.ino
@@ -1,29 +1,31 @@
-#include <SoftwareSerial.h>
+#include <SoftwareSerial.h> 
 
-SoftwareSerial btSerial(2,3);
 
-void setup()
+
+SoftwareSerial BTSerial(2, 3);  //객체 지향 언어의 형태이고, 해당라이브러리를 사용하겠다고 선언한 내용입니다.
+ 
+void setup()  
 {
   Serial.begin(9600);
-  btSerial.begin(9600);
-}
+  Serial.println("Hello!");
+ 
+  BTSerial.begin(9600);  // set the data rate for the BT port
+  pinMode(13, OUTPUT);
 
+
+}
+ 
 void loop()
 {
-    if(Serial.available())
-    {
-      delay(5); 
-      while(Serial.available())
-      {
-        btSerial.write(Serial.read());
-      }
+  byte data;
+  data  =  BTSerial.read();
+    if(data=='a'){
+      digitalWrite(13, HIGH);   // set the LED on
+      Serial.println("a");
     }
-    if(btSerial.available())
-    {
-      delay(5); 
-      while(btSerial.available())
-      {
-        Serial.write(btSerial.read());
-      }
+    if(data=='b'){
+      digitalWrite(13, LOW);    // set the LED off
+      Serial.println("b");
     }
+
 }
